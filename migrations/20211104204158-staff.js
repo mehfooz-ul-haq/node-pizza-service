ac35c177e95293f56ed00a41cb0b63edaa4177dd
Mehhfooz <mehfooz@tpconnets.com>
2021-11-05 01:07:13 +0400

    create migration

--- migrations/20211104204158-staff.js
+++ migrations/20211104204158-staff.js
@@ -3,8 +3,48 @@ module.exports = {
         // TODO write your migration here.
         // See https://github.com/seppevs/migrate-mongo/#creating-a-new-migration-script
         // Example:
-        await db.collection('staffssss').insert(
-            {artist: 'The Beatles'}
+        await db.collection('staff').insertMany(
+            [{
+                name: "Chef 1",
+                type: "Dough",
+                timeToTakeInSeconds: 7,
+                isBusy: false,
+            },{
+                name: "Chef 2",
+                type: "Dough",
+                timeToTakeInSeconds: 7,
+                isBusy: false,
+            },{
+                name: "Chef 1",
+                type: "Topping",
+                timeToTakeInSeconds: 4,
+                isBusy: false,
+            },{
+                name: "Chef 2",
+                type: "Topping",
+                timeToTakeInSeconds: 4,
+                isBusy: false,
+            },{
+                name: "Chef 3",
+                type: "Topping",
+                timeToTakeInSeconds: 4,
+                isBusy: false,
+            },{
+                name: "Oven 1",
+                type: "Oven",
+                timeToTakeInSeconds: 10,
+                isBusy: false,
+            },{
+                name: "Waiter 1",
+                type: "Waiter",
+                timeToTakeInSeconds: 5,
+                isBusy: false,
+            },{
+                name: "Waiter 2",
+                type: "Waiter",
+                timeToTakeInSeconds: 5,
+                isBusy: false,
+            }]
         );
     },
 
